13596c1326a015a0daeadfc19ba03f54c407feea
YarascaB <72763584@continental.edu.pe>
2026-06-09 11:23:29 -0500

    Agregar recuperacion de contraseña

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -60,4 +60,67 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.put("/reset-password", async (req, res) => {
+
+  try {
+
+    const {
+      email,
+      password,
+    } = req.body;
+
+    const existe = await pool.query(
+
+      `
+      SELECT id
+      FROM usuarios_mock
+      WHERE email = $1
+      `,
+
+      [email]
+    );
+
+    if (existe.rows.length === 0) {
+
+      return res.status(404).json({
+
+        success: false,
+
+        message:
+          "Correo no encontrado",
+      });
+    }
+
+    await pool.query(
+
+      `
+      UPDATE usuarios_mock
+      SET password_hash = $1
+      WHERE email = $2
+      `,
+
+      [password, email]
+    );
+
+    res.json({
+
+      success: true,
+
+      message:
+        "Contraseña actualizada correctamente",
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+
+      success: false,
+
+      error: error.message,
+    });
+  }
+});
+
 module.exports = router;
